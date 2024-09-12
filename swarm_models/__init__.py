6c9f76d406b7d127a3ccca2b75e30396c07d16e6
Your Name <you@example.com>
2024-09-12 16:41:23 -0400

    [CLEANUP]

---
+++ swarm_models/__init__.py
@@ -0,0 +1,80 @@
+from swarm_models.base_embedding_model import BaseEmbeddingModel
+from swarm_models.base_llm import BaseLLM  # noqa: E402
+from swarm_models.base_multimodal_model import BaseMultiModalModel
+from swarm_models.fuyu import Fuyu  # noqa: E402
+from swarm_models.gpt4_vision_api import GPT4VisionAPI  # noqa: E402
+from swarm_models.huggingface import HuggingfaceLLM  # noqa: E402
+from swarm_models.idefics import Idefics  # noqa: E402
+from swarm_models.kosmos_two import Kosmos  # noqa: E402
+from swarm_models.layoutlm_document_qa import LayoutLMDocumentQA
+from swarm_models.llama3_hosted import llama3Hosted
+from swarm_models.llava import LavaMultiModal  # noqa: E402
+from swarm_models.nougat import Nougat  # noqa: E402
+from swarm_models.openai_embeddings import OpenAIEmbeddings
+from swarm_models.openai_tts import OpenAITTS  # noqa: E402
+from swarm_models.palm import GooglePalm as Palm  # noqa: E402
+from swarm_models.popular_llms import Anthropic as Anthropic
+from swarm_models.popular_llms import (
+    AzureOpenAILLM as AzureOpenAI,
+)
+from swarm_models.popular_llms import (
+    CohereChat as Cohere,
+)
+from swarm_models.popular_llms import OctoAIChat
+from swarm_models.popular_llms import (
+    OpenAIChatLLM as OpenAIChat,
+)
+from swarm_models.popular_llms import (
+    OpenAILLM as OpenAI,
+)
+from swarm_models.popular_llms import ReplicateChat as Replicate
+from swarm_models.qwen import QwenVLMultiModal  # noqa: E402
+from swarm_models.sampling_params import SamplingParams, SamplingType
+from swarm_models.together import TogetherLLM  # noqa: E402
+from swarm_models.model_types import (  # noqa: E402
+    AudioModality,
+    ImageModality,
+    MultimodalData,
+    TextModality,
+    VideoModality,
+)
+from swarm_models.vilt import Vilt  # noqa: E402
+from swarm_models.popular_llms import FireWorksAI
+from swarm_models.openai_function_caller import OpenAIFunctionCaller
+
+__all__ = [
+    "BaseEmbeddingModel",
+    "BaseLLM",
+    "BaseMultiModalModel",
+    "Fuyu",
+    "GPT4VisionAPI",
+    "HuggingfaceLLM",
+    "Idefics",
+    "Kosmos",
+    "LayoutLMDocumentQA",
+    "LavaMultiModal",
+    "Nougat",
+    "Palm",
+    "OpenAITTS",
+    "Anthropic",
+    "AzureOpenAI",
+    "Cohere",
+    "OpenAIChat",
+    "OpenAI",
+    "OctoAIChat",
+    "QwenVLMultiModal",
+    "Replicate",
+    "SamplingParams",
+    "SamplingType",
+    "TogetherLLM",
+    "AudioModality",
+    "ImageModality",
+    "MultimodalData",
+    "TextModality",
+    "VideoModality",
+    "Vilt",
+    "OpenAIEmbeddings",
+    "llama3Hosted",
+    "FireWorksAI",
+    "OpenAIFunctionCaller",
+]
